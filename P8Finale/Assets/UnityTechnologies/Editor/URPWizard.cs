bff4c0429bc4d34cd6ab5d875aa440e89e899612
KaosStorm <145504760+KaosStorm@users.noreply.github.com>
2024-05-23 15:10:27 -0700

    Imported assest
    Added assets that will be used in game :0

---
+++ P8Finale/Assets/UnityTechnologies/Editor/URPWizard.cs
@@ -0,0 +1,8 @@
+using System.Linq;
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Rendering;
+#if USE_URP
+using UnityEngine.Rendering.Universal;
+#endif
